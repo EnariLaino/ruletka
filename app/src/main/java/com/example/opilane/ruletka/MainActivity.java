f02d1a85da7a7a78b7d65fa8b392bd0891192944
hendrikrandla <hendrik.randla@gmail.com>
2018-06-14 13:46:41 +0300

    25

--- app/src/main/java/com/example/opilane/ruletka/MainActivity.java
+++ app/src/main/java/com/example/opilane/ruletka/MainActivity.java
@@ -24,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
     int degree = 0, degree_old = 0;
 
+
+    private static final float FACTOR = 4.86f;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,12 +51,12 @@ public class MainActivity extends AppCompatActivity {
                 rotate.setAnimationListener(new Animation.AnimationListener() {
                     @Override
                     public void onAnimationStart(Animation animation) {
-
+                        textView.setText("");
                     }
 
                     @Override
                     public void onAnimationEnd(Animation animation) {
-
+                        textView.setText(currentNumber(360 - (degree % 360)));
                     }
 
                     @Override
@@ -65,5 +68,123 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private String currentNumber(int degrees){
+        String text = "";
+
+        if(degrees >= (FACTOR * 1) && degrees < (FACTOR * 3)){
+            text = "32 punane";
+        }
+        if(degrees >= (FACTOR * 3) && degrees < (FACTOR * 5)){
+            text = "15 must";
+        }
+        if(degrees >= (FACTOR * 5) && degrees < (FACTOR * 7)){
+            text = "19 punane";
+        }
+        if(degrees >= (FACTOR * 7) && degrees < (FACTOR * 9)){
+            text = "4 must";
+        }
+        if(degrees >= (FACTOR * 9) && degrees < (FACTOR * 11)){
+            text = "21 punane";
+        }
+        if(degrees >= (FACTOR * 11) && degrees < (FACTOR * 13)){
+            text = "2 must";
+        }
+        if(degrees >= (FACTOR * 13) && degrees < (FACTOR * 15)){
+            text = "25 punane";
+        }
+        if(degrees >= (FACTOR * 15) && degrees < (FACTOR * 17)){
+            text = "17 must";
+        }
+        if(degrees >= (FACTOR * 17) && degrees < (FACTOR * 19)){
+            text = "34 punane";
+        }
+        if(degrees >= (FACTOR * 19) && degrees < (FACTOR * 21)){
+            text = "6 must";
+        }
+        if(degrees >= (FACTOR * 21) && degrees < (FACTOR * 23)){
+            text = "27 punane";
+        }
+        if(degrees >= (FACTOR * 23) && degrees < (FACTOR * 25)){
+            text = "13 must";
+        }
+        if(degrees >= (FACTOR * 25) && degrees < (FACTOR * 27)){
+            text = "36 punane";
+        }
+        if(degrees >= (FACTOR * 27) && degrees < (FACTOR * 29)){
+            text = "11 must";
+        }
+        if(degrees >= (FACTOR * 29) && degrees < (FACTOR * 31)){
+            text = "30 punane";
+        }
+        if(degrees >= (FACTOR * 31) && degrees < (FACTOR * 33)){
+            text = "8 must";
+        }
+        if(degrees >= (FACTOR * 33) && degrees < (FACTOR * 35)){
+            text = "23 punane";
+        }
+        if(degrees >= (FACTOR * 35) && degrees < (FACTOR * 37)){
+            text = "10 must";
+        }
+        if(degrees >= (FACTOR * 37) && degrees < (FACTOR * 39)){
+            text = "5 punane";
+        }
+        if(degrees >= (FACTOR * 39) && degrees < (FACTOR * 41)){
+            text = "24 must";
+        }
+        if(degrees >= (FACTOR * 41) && degrees < (FACTOR * 43)){
+            text = "16 punane";
+        }
+        if(degrees >= (FACTOR * 43) && degrees < (FACTOR * 45)){
+            text = "33 must";
+        }
+        if(degrees >= (FACTOR * 45) && degrees < (FACTOR * 47)){
+            text = "1 punane";
+        }
+        if(degrees >= (FACTOR * 47) && degrees < (FACTOR * 49)){
+            text = "20 must";
+        }
+        if(degrees >= (FACTOR * 49) && degrees < (FACTOR * 51)){
+            text = "14 punane";
+        }
+        if(degrees >= (FACTOR * 51) && degrees < (FACTOR * 53)){
+            text = "31 must";
+        }
+        if(degrees >= (FACTOR * 53) && degrees < (FACTOR * 55)){
+            text = "9 punane";
+        }
+        if(degrees >= (FACTOR * 55) && degrees < (FACTOR * 57)){
+            text = "22 must";
+        }
+        if(degrees >= (FACTOR * 57) && degrees < (FACTOR * 59)){
+            text = "18 punane";
+        }
+        if(degrees >= (FACTOR * 59) && degrees < (FACTOR * 61)){
+            text = "29 must";
+        }
+        if(degrees >= (FACTOR * 61) && degrees < (FACTOR * 63)){
+            text = "7 punane";
+        }
+        if(degrees >= (FACTOR * 63) && degrees < (FACTOR * 65)){
+            text = "28 must";
+        }
+        if(degrees >= (FACTOR * 65) && degrees < (FACTOR * 67)){
+            text = "12 punane";
+        }
+        if(degrees >= (FACTOR * 67) && degrees < (FACTOR * 69)){
+            text = "35 must";
+        }
+        if(degrees >= (FACTOR * 69) && degrees < (FACTOR * 71)){
+            text = "3 punane";
+        }
+        if(degrees >= (FACTOR * 71) && degrees < (FACTOR * 73)) {
+            text = "26 must";
+        }
+        if ((degrees >= (FACTOR * 73) && degrees < 360) || (degrees >= 0 && degrees < (FACTOR * 1))){
+            text = "0";
+        }
+
+        return text;
     }
 }
